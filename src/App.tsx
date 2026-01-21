743a277a7e8417193f77e1d52235f779be04e23a
Jaime Sánchez Blanco <jaimeprogramador83@gmail.com>
2026-01-21 08:42:13 +0100

    Initial commit: React + TypeScript project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>This is an empty React project ready for development.</p>
+    </div>
+  )
+}
+
+export default App
